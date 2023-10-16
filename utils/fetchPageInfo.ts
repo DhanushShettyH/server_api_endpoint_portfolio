49ee2f75296d40c605a367f7173e592ef16b8c4d
dhanush <dhanushtruth@gmail.com>
2023-10-16 13:34:32 +0530

    done

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -3,7 +3,7 @@ import { PageInfo } from "../typings";
 
 export const  fetchPageInfo = async()=> {
     // here making call to backend
-    const res = await fetch(`http://localhost:3000//api/getPageInfo`);
+    const res = await fetch(`http://127.0.0.1/api/getPageInfo`);
     const data = await res.json()
     // typing checking data
     const pageInfo: PageInfo = data.pageInfo;

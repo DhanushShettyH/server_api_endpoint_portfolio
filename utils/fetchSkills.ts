7bf1c554895eb8492e1f15977920dc69f0859e29
dhanush <dhanushtruth@gmail.com>
2023-10-16 13:00:59 +0530

    done

--- utils/fetchSkills.ts
+++ utils/fetchSkills.ts
@@ -2,7 +2,7 @@ import { Skill } from "../typings";
 
 export const  fetchSkills = async()=> {
     // here making call to backend
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
+    const res = await fetch(`http://localhost:3000/api/getSkills`);
     const data = await res.json()
     // typing checking data
     const skills: Skill[]= data.skills;
--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -4,7 +4,7 @@ import { Social } from "../typings";
 
 export const  fetchSocials = async()=> {
     // here making call to backend
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
+    const res = await fetch(`http://localhost:3000/api/getSocials`);
     const data = await res.json()
     // typing checking data
     const socials: Social[]= data.socials;
